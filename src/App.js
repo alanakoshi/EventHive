badbdacc5d8fb553256fbeaccc24a1fb0ee7c727
Alana Koshikawa <alanakoshikawa@gmail.com>
2025-03-25 13:16:46 -0500

    added pages

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Plan from "./Plan";
+import Invites from "./Invites";
+import Events from "./Events";
+import Money from "./Money";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/plan" element={<Plan />} />
+        <Route path="/invites" element={<Invites />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/money" element={<Money />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Events.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Events() {
+  return (
+    <div>
+      <h2>Events Page</h2>
+      <p>This is the Plan page content.</p>
+    </div>
+  );
+}
+
+export default Events;
